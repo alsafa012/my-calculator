8fd8d7fe0d35d2f3c53b1d0608cfa3333e791579
alsafa012 <alsafa012@gmail.com>
2023-08-19 16:49:59 +0600

    all complete using div

--- js/styles.js
+++ js/styles.js
@@ -1,72 +1,26 @@
+const addDisplay = document.getElementById("display");
+//  const addDisplay = document.calculator.display;
 
-const display = document.getElementById('input'); // Corrected selector
-
-function calculatePercentage() {
-    try {
-        display.value = eval(display.value) / 100;
-    } catch (error) {
-        display.value = 'Error';
-    }
+function addToDisplay(char) {
+     addDisplay.value += char;
 }
-
-const buttons = document.querySelectorAll('.btn');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const buttonText = button.value;
-
-        if (buttonText === '=') {
-            try {
-                display.value = eval(display.value);
-            } catch (error) {
-                display.value = 'Error';
-            }
-        } else if (buttonText === 'C') {
-            display.value = '';
-        } else if (buttonText === 'DEL') {
-            display.value = display.value.slice(0, -1);
-        } else if (buttonText !== '+/-' && buttonText !== '%') {
-            display.value += buttonText;
-        }
-    });
-});
-
-/* 
-// Using String.prototype
-const display = document.getElementById('input');
-
-function calculatePercentage() {
-    try {
-        const inputValue = parseFloat(display.value);
-        display.value = (inputValue / 100).toString();
-    } catch (error) {
-        display.value = 'Error';
-    }
+function clearDisplay(){
+     document.getElementById("display").value = "";
 }
-
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const buttonText = button.value;
-
-        if (buttonText === '=') {
-            try {
-                const result = evaluateExpression(display.value);
-                display.value = result.toString();
-            } catch (error) {
-                display.value = 'Error';
-            }
-        } else if (buttonText === 'C') {
-            display.value = '';
-        } else if (buttonText === 'DEL') {
-            display.value = display.value.slice(0, -1);
-        } else {
-            display.value += buttonText;
-        }
-    });
-});
-
-function evaluateExpression(expression) {
-    return Function('"use strict";return (' + expression + ')')();
+function del(){
+     addDisplay.value = addDisplay.value.slice(0,-1);
 }
- */
+function calculate(){
+     try{
+          addDisplay.value = eval(addDisplay.value)
+     }catch(error){
+          addDisplay.value = 'ERROR'
+     }
+}
+function parentageCalculate(){
+     try{
+          addDisplay.value = eval(addDisplay.value / 100) ;
+     }catch{
+          addDisplay.value = 'Error';
+     }
+}
